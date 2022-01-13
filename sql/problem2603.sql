8101ce949b98e4475c766b87ea378536ff6ad061
Thiago Rodrigues <thiagoney@outlook.com>
2022-01-13 02:27:52 -0300

    Update files problem2602.sql to problem2604.sql

--- sql/problem2603.sql
+++ sql/problem2603.sql
@@ -1,2 +1,25 @@
-SELECT name, street FROM customers
-WHERE city = 'Porto Alegre'
+CREATE DATABASE beecrowd2603;
+
+USE beecrowd2603;
+
+CREATE TABLE customers (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+street VARCHAR(50) NOT NULL,
+city VARCHAR(50) NOT NULL,
+state CHAR(2) NOT NULL,
+credit_limit DECIMAL(8,2) NOT NULL
+);
+
+INSERT INTO customers (name, street, city, state, credit_limit)
+VALUES ('Pedro Augusto da Rocha', 'Rua Pedro Carlos Hoffman', 'Porto Alegre', 'RS', '700.00');
+INSERT INTO customers (name, street, city, state, credit_limit)
+VALUES ('Antonio Carlos Mamel', 'Av. Pinheiros', 'Belo Horizonte', 'MG', '3500.50');
+INSERT INTO customers (name, street, city, state, credit_limit)
+VALUES ('Luiza Augusta Mhor', 'Rua Salto Grande', 'Niteroi', 'RJ', '4000.00');
+INSERT INTO customers (name, street, city, state, credit_limit)
+VALUES ('Jane Ester', 'Av 7 de setembro', 'Erechim', 'RS', '800.00');
+INSERT INTO customers (name, street, city, state, credit_limit)
+VALUES ('Marcos Antônio dos Santos', 'Av Farrapos', 'Porto Alegre', 'RS', '4250.25');
+
+SELECT name, street FROM customers WHERE city = 'Porto Alegre';
--- sql/problem2604.sql
+++ sql/problem2604.sql
@@ -1,2 +1,19 @@
-SELECT id, name FROM products
-WHERE (price < 10) OR (price > 100)
+CREATE DATABASE beecrowd2604;
+
+USE beecrowd2604;
+
+CREATE TABLE products (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50),
+amount NUMERIC,
+price DECIMAL(8,2)
+);
+
+INSERT INTO products (name, amount, price) VALUES ('Two-door wardrobe', '100', '80');
+INSERT INTO products (name, amount, price) VALUES ('Dining table', '1000', '560');
+INSERT INTO products (name, amount, price) VALUES ('Towel holder', '10000', '5.50');
+INSERT INTO products (name, amount, price) VALUES ('Computer desk', '350', '100');
+INSERT INTO products (name, amount, price) VALUES ('Chair', '3000', '210.64');
+INSERT INTO products (name, amount, price) VALUES ('Single bed', '750', '99');
+
+SELECT id, name FROM products WHERE price < 10 OR price > 100;
